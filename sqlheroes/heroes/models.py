e4acc051ea2ec032679eb495f0445ceb99017fd5
John-Michael Crawford <jm.crawford@student.awesomeinc.org>
2023-03-28 19:11:19 +0000

    manytomany|heroes->abilitytype through abilitytype foreign key relationship -> relationshiptype foreign key relationship -> relationshiptype foreign key hero1/hero2 -> hero using relatedname

--- sqlheroes/heroes/models.py
+++ sqlheroes/heroes/models.py
@@ -1,14 +1,30 @@
 from django.db import models
 
 # Create your models here.
-class Heroes(models.Model):
-    name = models.CharField(max_length=50)
-    about_me = models.CharField(max_length=100)
-    bio = models.CharField(max_length=300)
+class Hero(models.Model):
+    name = models.CharField(max_length = 50)
+    about_me = models.CharField(max_length = 100)
+    bio = models.CharField(max_length = 300)
+    ability_types = models.ManyToManyField('AbilityType', through='Ability')
 
-#class Ability_Types(models.Model):
-    #pass
+class Ability(models.Model):
+    hero = models.ForeignKey('Hero', on_delete=models.PROTECT, null=True)
+    ability_type = models.ForeignKey('AbilityType', on_delete=models.PROTECT, null=True)
+    
 
-#class Relationship_Types(models.Model):
-    #pass
+class AbilityType(models.Model):
+    name = models.CharField(max_length = 50, null=True)
+
+
+class Relationship(models.Model):
+    hero1 = models.ForeignKey('Hero', on_delete=models.PROTECT, null=True, related_name='hero1')
+    hero2 = models.ForeignKey('Hero', on_delete=models.PROTECT, null=True, related_name='hero2')
+    relationship_type = models.ForeignKey('RelationshipType', on_delete=models.PROTECT, null=True)
+
+class RelationshipType(models.Model):
+    name = models.CharField(max_length = 50, null=True)
+
+
+
+
 
